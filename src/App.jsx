2a7ce80a6cf044393613cc79ba6a1ea2166b2cbf
DylanBk <d.bullock1404@outlook.com>
2025-03-03 14:50:53 +0000

    added gradient selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import Overview from "./components/Overview";
 
 export default function App() {
   const apiKey = import.meta.env.VITE_API_KEY
-  const [userLocation, setUserLocation] = useState(null)
+
+  const [userLocation, setUserLocation] = useState('')
   const [weatherData, setWeatherData] = useState({})
-  const [theme, setTheme] = useState(null)
+  const [theme, setTheme] = useState('bg-[#f5f5f5]');
 
   useEffect(() => {
     const reverseGeocode = async (lat, lon) => { // uses latitude and longitude to find user's closest settlement
@@ -17,14 +18,14 @@ export default function App() {
         })
         .then(res => res.json());
 
-        return ( // returns location name or unknown if it cant find it
+        return ( // returns location name or none if it cant find it
           res.address.city ||
           res.address.town ||
           res.address.village ||
           res.address.suburb || 
           res.address.county || 
           res.address.state || 
-          'Unknown Location'
+          null
         );
       } catch(err) {
         console.error(err);
@@ -43,13 +44,16 @@ export default function App() {
         const locationName = await reverseGeocode(lat, lon);
 
         setUserLocation(locationName);
-
       } catch (err) {
         console.error(err);
-      }
+      };
     };
 
-    const getWeatherData = async (userLocation) => { // uses the settlement returned from reverseGeocode to fetch the weather for that area
+    getUserLocation()
+  }, []);
+
+  useEffect(() => {
+    const getWeatherData = async () => { // uses the settlement returned from reverseGeocode to fetch the weather for that area
       try {
         const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${userLocation}&aqi=yes`;
         const res = await fetch(url, {
@@ -57,26 +61,64 @@ export default function App() {
         })
         .then(res => res.json());
 
-        if (res.ok) {
-          console.log(res)
-          setWeatherData(res);
+        if (res.current) {
+          setWeatherData(res.current);
+          selectTheme(res.current.condition.code)
         };
       } catch(err) {
         console.error(err);
       };
     };
 
-    getUserLocation();
-
     if (userLocation) {
-      getWeatherData(userLocation);
+      getWeatherData();
     };
-  });
+  }, [userLocation]);
 
+  const selectTheme = (code) => {
+    // build tailwind gradient based on weather conditions
+    //   --color-grey: #ccc;
+    // --color-sun: #CDAD5A;
+    // --color-sky-electric: #0874C1;
+    // --color-sky-neutral: #6495ED;
+    // --color-sky-dull: #81A5E7;
+    // --color-cloud-light: #AAAAAA;
+    // --color-cloud-dark: #454D54;
+
+    const clearArr = [1000, 1003];
+    const overcastArr = [1006, 1009, 1030, 1135, 1147];
+    const darkCloudArr = [1063, 1072, 1150, 1153, 1180, 1183, 1186, 1189, 1192, 1195, 1198, 1201, 1240, 1243, 1246, 1273, 1276, 1279, 1282, 1114, 1117, 1171, 1258, 1264];
+    const snowArr = [1066, 1069, 1072, 1168, 1171, 1204, 1207, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1255, 1258, 1261, 1264];
+
+    const clear = 'bg-gradient-to-b from-sun via-sky-neutral to-sky-electric';
+    const overcast = 'bg-gradient-to-b from-sky-dull to-cloud-light';
+    const darkCloud = 'bg-gradient-to-b from-cloud-dark to-cloud-light';
+    const snow = 'bg-gradient-to-b from-cloud-light to-snow';
+
+    console.log('code', code)
+
+    switch (true) {
+      case clearArr.includes(code):
+        setTheme(clear)
+        break;
+      case overcastArr.includes(code):
+        setTheme(overcast)
+        break;
+      case darkCloudArr.includes(code):
+        setTheme(darkCloud)
+        break;
+      case snowArr.includes(code):
+        setTheme(snow)
+        break;
+    }
+
+    console.log(theme)
+  };
 
   return (
-    <div className={theme}>
-      <h1 className="text-5xl text-white">Weatherly</h1>
+    <div className={`h-screen w-full ${theme}`}>
+      <h1 className="w-full p-4 text-5xl text-white">Weatherly</h1>
+
       <Overview weatherData={weatherData} />
     </div>
   );
